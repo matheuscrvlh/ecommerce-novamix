92a9e70b014afd856d5f6ef74ad01e2457d53aad
Matheus Carvalho <matheusscarvlh@gmail.com>
2026-06-17 15:28:34 -0300

    primeiro commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+  const [a,aa] = useState()
+  return (
+    <>
+    <div className='bg-amber-50'></div>
+    </>
+  )
+}
+
+export default App
