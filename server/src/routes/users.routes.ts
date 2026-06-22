70397d8c84df0bd03f24e8533ad1c49b730656c5
Matheus Carvalho <matheusscarvlh@gmail.com>
2026-06-21 23:05:00 -0300

    feat: adiciona rotas get,delete e put em usuarios

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -4,10 +4,12 @@ import { hashPassword } from '../utils/hash.js'
 import { authenticate, checkAdmin } from '../middlewares/auth.middleware.js'
 
 type CreateUserBody = {
+    id: number
     nome: string
     login: string
     senha: string
     role: string
+    status: boolean
 }
 
 async function createUser(req: FastifyRequest<{Body: CreateUserBody}>, res: FastifyReply) {
@@ -27,7 +29,50 @@ async function createUser(req: FastifyRequest<{Body: CreateUserBody}>, res: Fast
     }
 }
 
+async function getUsers(res:FastifyReply) {
+    const result = await db.query(
+        'SELECT * FROM usuarios'
+    );
+
+    res.send(result.rows)
+}
+
+async function putUser(req:FastifyRequest<{Body: CreateUserBody}>, res:FastifyReply) {
+    const { id, nome, login, senha, role, status } = req.body
+
+    const passwordHashed = await hashPassword(senha)
+
+    const result = db.query(
+        'UPDATE usuarios SET nome = $1, login = $2, senha = $3, role = $4, status = $5 WHERE id = $6',
+        [nome, login, passwordHashed, role, status, id]
+    );
+
+    if(!result) {
+        throw new Error('Nenhum usuário encontrado.')
+    }
+
+    return res.code(201).send({ success: 'Usuário editado com sucesso.'})
+}
+
+async function deleteUser(req:FastifyRequest, res:FastifyReply) {
+    const { id } = req.body
+
+    const result = await db.query(
+        'DELETE FROM usuarios WHERE id = $1',
+        [id]
+    );
+
+    if(!result) {
+        return res.code(401).send({ error: 'Erro ao deletar usuário.'})
+    };
+
+    return res.code(201).send({ success: 'Usuário deletado com sucesso'})
+}
+
 export async function usersRoutes(fastify: FastifyInstance) {
-    fastify.post('/usuarios', { preHandler: [authenticate, checkAdmin] }, createUser)
+    fastify.post('/usuarios', { preHandler: [authenticate, checkAdmin] }, createUser);
+    fastify.get('/usuarios', { preHandler: [authenticate, checkAdmin] }, getUsers);
+    fastify.put('/usuarios', { preHandler: [authenticate, checkAdmin] }, putUser);
+    fastify.delete('/usuarios', { preHandler: [authenticate, checkAdmin] }, deleteUser);
 }
 
