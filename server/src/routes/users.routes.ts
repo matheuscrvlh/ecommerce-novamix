1ef29dc5633f0e0141150ddfaa596424fa2679b9
Matheus Carvalho <matheusscarvlh@gmail.com>
2026-07-09 17:29:16 -0300

    fix: corrige metade da rota de edit user

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -7,7 +7,7 @@ type CreateUserBody = {
     id: number
     nome: string
     login: string
-    senha: string
+    senha?: string
     role: string
     cracha: string
     status: boolean
@@ -41,17 +41,20 @@ async function getUsers(req:FastifyRequest, res:FastifyReply) {
 async function putUser(req:FastifyRequest<{Body: CreateUserBody}>, res:FastifyReply) {
     const { id, nome, login, senha, role, cracha, status } = req.body
 
-    const passwordHashed = await hashPassword(senha)
+    if (senha) {
+        const passwordHashed = await hashPassword(senha);
 
-    const result = await db.query(
-        'UPDATE usuarios SET nome = $1, login = $2, senha = $3, role = $4, cracha = $5, status = $6 WHERE id = $7',
-        [nome, login, passwordHashed, role, cracha, status, id]
-    );
+        const result = await db.query(
+            'UPDATE usuarios SET nome = $1, login = $2, senha = $3, role = $4, cracha = $5, status = $6 WHERE id = $7',
+            [nome, login, passwordHashed, role, cracha, status, id]
+        );
 
-    if(result.rowCount === 0) {
-        throw new Error('Nenhum usuário encontrado.')
+        if(result.rowCount === 0) {
+            throw new Error('Nenhum usuário encontrado.')
+        };
     }
 
+    
     return res.code(200).send({ success: 'Usuário editado com sucesso.'})
 }
 
